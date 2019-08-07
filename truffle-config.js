3bb5b444069f35d05027bd997ac6f84366bb0b16
Your Name <muhdsyahrulnizam123@gmail.com>
2019-08-07 14:40:51 +0800

    added git ignore

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,20 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const { mnemonic, rpc } = require('./password.js')
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+	networks: {
+		development: {
+			host: "127.0.0.1",
+			port: 8545,
+			network_id: "*"
+		},
+		quorum: {
+			provider: function() {
+				return new HDWalletProvider(mnemonic,rpc)
+			},
+			network_id:'32800',
+			gasPrice:0,
+			type:"quorum"
+		}
+	},
 };
